1b136d1371b4f4760a863ed624e7bf36053bc448
kaiweicai <kaiweicai@163.com>
2024-08-03 18:27:58 +0800

    add test_play_fail

--- session/src/lib.rs
+++ session/src/lib.rs
@@ -8,7 +8,6 @@ const WORD_LENGTH:usize = 5;
 pub struct Session {
     wordle: ActorId,
     player_game_status:HashMap<ActorId, GameStatus>,
-    player_message_id:HashMap<MessageId,ActorId>,
     //记录一个session用户参加次数
     player_times:HashMap<ActorId,Vec<String>>,
     max_play_times:u32,
@@ -45,7 +44,6 @@ pub extern "C" fn init() {
         SESSION = Some(Session {
             wordle: game_session_init.wordle_address,
             player_game_status: HashMap::new(),
-            player_message_id: HashMap::new(),
             player_times: HashMap::new(),
             max_play_times: game_session_init.max_play_times
         });
@@ -68,13 +66,11 @@ extern fn handle() {
             Action::StartGame { user } => {
                 let send_msg_id = msg::send(session.wordle, user_action, 0).expect("Failed to send");
                 let origin_id = msg::id();
-                session.player_message_id.insert(origin_id, user_id);
                 session.player_game_status.insert(user_id, GameStatus::StartGameMessageSend {
                     origin_id,
                     send_id: send_msg_id
                 });
                 debug!("origin_id is:{:?}",origin_id);
-                debug!("session.player_message_id is:{:?}",&session.player_message_id);
                 exec::wait();
             },
 
@@ -83,10 +79,10 @@ extern fn handle() {
                 debug!("word.capacity() is:{}",word.capacity());
                 assert_eq!(word.capacity(), WORD_LENGTH, "The length of the word exceeds 6");
                 session.player_times.entry(user_id).or_insert_with(Vec::<String>::new).push(word);
+                debug!("check world session.player_times is:{:?}",session.player_times);
                 let send_msg_id = msg::send(session.wordle, user_action, 0).expect("Failed to send");
                 debug!("start check word send_msg_id is:{:?}",send_msg_id);
                 let origin_id = msg::id();
-                session.player_message_id.insert(origin_id, user_id);
                 session.player_game_status.insert(user_id, GameStatus::CheckWordMessageSend {
                     origin_id,
                     send_id: send_msg_id
@@ -115,14 +111,14 @@ extern fn handle() {
                 debug!("check word event is:{:?}",event);
                 //清空用户的状态
                 session.player_game_status.remove(&user_id);
-                session.player_message_id.remove(&msg_id);
-                session.player_times.remove(&user_id);
                 //检查用户是否结束了游戏
                 match event.clone() {
                     Event::WordChecked{ user,
                         correct_positions,
                         contained_in_word,} => {
-                        if !correct_positions.contains(&0)||session.player_times.get(&user_id).unwrap().len() as u32==session.max_play_times{//游戏结束
+                        debug!("session.player_times.get(&user_id).expect(\"Failed to get times\").len() is :{:?}",session.player_times.get(&user_id));
+                        if !(correct_positions.contains(&0)||session.player_times.get(&user_id).expect("Failed to get times").len() as u32==session.max_play_times){//游戏结束
+                            session.player_times.remove(&user_id);
                             msg::reply(Event::UserWin {user},0).expect("Failed to reply");
                             return;
                         }
--- session/tests/tests.rs
+++ session/tests/tests.rs
@@ -81,11 +81,61 @@ pub fn test_play_success() {
     );
 
     assert!(!success_wordle_result.main_failed(), "wordle run failed");
-    // assert!(
-    //     success_wordle_result.contains(&Log::builder().payload(Event::WordChecked {
-    //         user: USER.into(),
-    //         correct_positions: vec![0, 1, 3, 4],
-    //         contained_in_word: vec![]
-    //     }))
-    // );
+    assert!(
+        success_wordle_result.contains(&Log::builder().payload(Event::WordChecked {
+            user: USER.into(),
+            correct_positions: vec![0, 1, 2,3, 4],
+            contained_in_word: vec![]
+        }))
+    );
+}
+
+#[test]
+pub fn test_play_fail() {
+    let system = System::new();
+    system.init_logger();
+
+    let (session_program, wordle_program) = init_game(&system);
+    let start_result = session_program.send(USER, StartGame { user: USER.into() });
+    assert!(!start_result.main_failed(), "start run failed");
+    let start_logs = start_result.log();
+    println!("start logs is:{:?}", start_logs);
+    assert!(
+        start_result.contains(&Log::builder().payload(Event::GameStarted { user: USER.into() })),
+        "receive log error!"
+    );
+
+    //start test wordle
+    let wordle_result = session_program.send(
+        USER,
+        CheckWord {
+            user: USER.into(),
+            word: "house".to_string(),
+        },
+    );
+    assert!(!wordle_result.main_failed(), "wordle run failed");
+    assert!(
+        wordle_result.contains(&Log::builder().payload(Event::WordChecked {
+            user: USER.into(),
+            correct_positions: vec![0, 1, 3, 4],
+            contained_in_word: vec![]
+        }))
+    );
+
+    let success_wordle_result = session_program.send(
+        USER,
+        CheckWord {
+            user: USER.into(),
+            word: "horse".to_string(),
+        },
+    );
+
+    assert!(!success_wordle_result.main_failed(), "wordle run failed");
+    assert!(
+        success_wordle_result.contains(&Log::builder().payload(Event::WordChecked {
+            user: USER.into(),
+            correct_positions: vec![0, 1, 2,3, 4],
+            contained_in_word: vec![]
+        }))
+    );
 }
